9a9ec75840582bca1a38f0821789803145e967d3
achyutjagini <achyut.jagini@gmail.com>
2023-04-08 12:08:02 +0530

    tailwind setup

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,14 +1,37 @@
-import { useContext } from "react";
-import { useHistory } from "react-router-dom";
+import { useContext, useState,useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import FirebaseContext from "../context/firebase"
 
 export default function Login()
 {
-const history=useHistory()
+const history=useNavigate()
 const {firebase}=useContext(FirebaseContext)
 
+const [emailAddress,setEmailAddress]=useState('');
+const [password,setPassword]=useState('');
+
+const [error,setError]=useState('')
+const isInvalid = password ===''||emailAddress===''
+
+const handleLogin=()=>{};
+
+useEffect(()=>{
+document.title='Login-instagram'
+},[]);
+
 return(
-<div>
-<p>I am the login page</p>
-</div>);
+<div className="container flex mx-auto max-w-screen-md items-center h-screen">
+      <div className="flex w-3/5">
+        <img src="/images/iphone-with-profile.jpg" alt="iPhone with Instagram app" />
+      </div>
+      <div className="flex flex-col w-2/5">
+        <div className="flex flex-col items-center bg-white p-4 border border-gray-primary mb-4 rounded">
+          <h1 className="flex justify-center w-full">
+            <img src="/images/logo.png" alt="Instagram" className="mt-2 w-6/12 mb-4" />
+          </h1>
+          {error && <p className="mb-4 text-xs text-red-primary">{error}</p>}
+</div>
+</div>
+</div>
+);
 }
